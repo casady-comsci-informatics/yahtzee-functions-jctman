1601de14539cd1088ad175a66cd77f2ff4a6be07
John <johntolbert@casady.org>
2019-03-06 09:55:27 -0600

    work

--- yahtzee.R
+++ yahtzee.R
@@ -3,11 +3,16 @@ roll <- function(die, size, replace){
 }
 
 yahtzeeRoll <- function(){
-
+  die <- c(1:6)
+  sample(die, size = 5, replace = TRUE)
 }
 
 sumRoll <- function(roll, value){
-
+  total = 0
+  for (value in roll) {
+    total = total + value
+  }
+  print(total)
 }
 
 isYahtzee(roll){
